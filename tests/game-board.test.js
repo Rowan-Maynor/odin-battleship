b2247105b6f98ede2bdeaf189858b05df57b870b
rowan-maynor <rowanmaynor@gmail.com>
2025-02-17 09:39:19 -0500

    update: Test cases now account for max ship size

--- tests/game-board.test.js
+++ tests/game-board.test.js
@@ -32,25 +32,37 @@ test("Throws error if initial placement is invalid", () => {
 });
 
 test("Creates ships pointing up", () => {
-  gameBoard.createShip(2, [5, 5], "up");
+  gameBoard.createShip(5, [5, 5], "up");
   expect(gameBoard.board[5][5]).toEqual(gameBoard.ships[0]);
   expect(gameBoard.board[4][5]).toEqual(gameBoard.ships[0]);
+  expect(gameBoard.board[3][5]).toEqual(gameBoard.ships[0]);
+  expect(gameBoard.board[2][5]).toEqual(gameBoard.ships[0]);
+  expect(gameBoard.board[1][5]).toEqual(gameBoard.ships[0]);
 });
 
 test("Creates ships pointing left", () => {
-  gameBoard.createShip(2, [5, 5], "left");
+  gameBoard.createShip(5, [5, 5], "left");
   expect(gameBoard.board[5][5]).toEqual(gameBoard.ships[0]);
   expect(gameBoard.board[5][4]).toEqual(gameBoard.ships[0]);
+  expect(gameBoard.board[5][3]).toEqual(gameBoard.ships[0]);
+  expect(gameBoard.board[5][2]).toEqual(gameBoard.ships[0]);
+  expect(gameBoard.board[5][1]).toEqual(gameBoard.ships[0]);
 });
 
 test("Creates ships pointing right", () => {
-  gameBoard.createShip(2, [5, 5], "right");
+  gameBoard.createShip(5, [5, 5], "right");
   expect(gameBoard.board[5][5]).toEqual(gameBoard.ships[0]);
   expect(gameBoard.board[5][6]).toEqual(gameBoard.ships[0]);
+  expect(gameBoard.board[5][7]).toEqual(gameBoard.ships[0]);
+  expect(gameBoard.board[5][8]).toEqual(gameBoard.ships[0]);
+  expect(gameBoard.board[5][9]).toEqual(gameBoard.ships[0]);
 });
 
 test("Creates ships pointing down", () => {
-  gameBoard.createShip(2, [5, 5], "down");
+  gameBoard.createShip(5, [5, 5], "down");
   expect(gameBoard.board[5][5]).toEqual(gameBoard.ships[0]);
   expect(gameBoard.board[6][5]).toEqual(gameBoard.ships[0]);
+  expect(gameBoard.board[7][5]).toEqual(gameBoard.ships[0]);
+  expect(gameBoard.board[8][5]).toEqual(gameBoard.ships[0]);
+  expect(gameBoard.board[9][5]).toEqual(gameBoard.ships[0]);
 });
