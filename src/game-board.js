83f49b928b3a1b2dc4d82e10100145080190f9ea
rowan-maynor <rowanmaynor@gmail.com>
2025-02-17 08:25:24 -0500

    feat: Update board location to match ship

--- src/game-board.js
+++ src/game-board.js
@@ -20,5 +20,7 @@ export class Gameboard {
   createShip(length, location, direction) {
     let newShip = new Ship(length);
     this.ships.push(newShip);
+
+    this.board[location[0], location[1]] = newShip;
   }
 }
