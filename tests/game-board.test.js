cda68e4b387475dbb5e6f97afd5f6d1654ead6fc
rowan-maynor <rowanmaynor@gmail.com>
2025-02-17 07:42:27 -0500

    fix: Change toBe assertions to toEqual

--- tests/game-board.test.js
+++ tests/game-board.test.js
@@ -7,7 +7,7 @@ beforeEach(() => {
 });
 
 test("Gameboard initializes correctly", ()=> {
-  expect(gameBoard.board).toBe([
+  expect(gameBoard.board).toEqual([
     [null, null, null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null, null, null],
@@ -23,24 +23,24 @@ test("Gameboard initializes correctly", ()=> {
 
 test("Creates ships pointing up", () => {
   gameBoard.createShip(2, [5][5], up);
-  expect(gameBoard.board[5][5]).toBe(gameBoard.ships[0]);
-  expect(gameBoard.board[4][5]).toBe(gameboard.ships[0]);
+  expect(gameBoard.board[5][5]).toEqual(gameBoard.ships[0]);
+  expect(gameBoard.board[4][5]).toEqual(gameboard.ships[0]);
 });
 
 test("Creates ships pointing left", () => {
   gameBoard.createShip(2, [5][5], left);
-  expect(gameBoard.board[5][5]).toBe(gameBoard.ships[0]);
-  expect(gameBoard.board[5][4]).toBe(gameboard.ships[0]);
+  expect(gameBoard.board[5][5]).toEqual(gameBoard.ships[0]);
+  expect(gameBoard.board[5][4]).toEqual(gameboard.ships[0]);
 });
 
 test("Creates ships pointing right", () => {
   gameBoard.createShip(2, [5][5], right);
-  expect(gameBoard.board[5][5]).toBe(gameBoard.ships[0]);
-  expect(gameBoard.board[5][6]).toBe(gameboard.ships[0]);
+  expect(gameBoard.board[5][5]).toEqual(gameBoard.ships[0]);
+  expect(gameBoard.board[5][6]).toEqual(gameboard.ships[0]);
 });
 
 test("Creates ships pointing down", () => {
   gameBoard.createShip(2, [5][5], down);
-  expect(gameBoard.board[5][5]).toBe(gameBoard.ships[0]);
-  expect(gameBoard.board[6][5]).toBe(gameboard.ships[0]);
+  expect(gameBoard.board[5][5]).toEqual(gameBoard.ships[0]);
+  expect(gameBoard.board[6][5]).toEqual(gameboard.ships[0]);
 });
